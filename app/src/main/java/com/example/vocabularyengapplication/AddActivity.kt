944e94a0382afc9268044d6ea5062f7663e706ab
Raka Pradana <raka.pradana1103@gmail.com>
2026-07-27 15:05:07 +0700

    feat(list): Implement Data Category List for Dashboard App

--- app/src/main/java/com/example/vocabularyengapplication/AddActivity.kt
+++ app/src/main/java/com/example/vocabularyengapplication/AddActivity.kt
@@ -1,18 +1,61 @@
 package com.example.vocabularyengapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.vocabularyengapplication.databinding.ActivityAddBinding
+import com.example.vocabularyengapplication.db.SqlDBHandler
+import com.example.vocabularyengapplication.model.WordCategory
 
 class AddActivity : AppCompatActivity() {
     private lateinit var bindingAddActivity: ActivityAddBinding
+    private val sqlHandler: SqlDBHandler = SqlDBHandler(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         bindingAddActivity = ActivityAddBinding.inflate(layoutInflater)
         setContentView(bindingAddActivity.root)
+        setSpinner()
+
+        bindingAddActivity.btnSave.setOnClickListener {
+            if (bindingAddActivity.etName.text.isNullOrEmpty() ||
+                bindingAddActivity.etMeaning.text.isNullOrEmpty()||
+                bindingAddActivity.spinnerCategory.selectedItem.toString().isEmpty()
+                ) return@setOnClickListener
+
+            sqlHandler.addVocab(
+                bindingAddActivity.etName.text.toString(),
+                bindingAddActivity.etMeaning.text.toString(),
+                bindingAddActivity.spinnerCategory.selectedItem.toString()
+            )
+            setResult(201, Intent())
+            finish()
+        }
+
+        bindingAddActivity.btnDiscard.setOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    private fun setSpinner(){
+        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(
+            this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, getCategoryList()
+        )
+        adapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item)
+        bindingAddActivity.spinnerCategory.adapter = adapter
+    }
+
+    private fun getCategoryList(): List<String>{
+        return WordCategory.values().map {
+            if (it == WordCategory.ALL_CATEGORIES){
+                ""
+            } else{
+                it.title
+            }
+        }
     }
 }
--- app/src/main/java/com/example/vocabularyengapplication/DashboardActivity.kt
+++ app/src/main/java/com/example/vocabularyengapplication/DashboardActivity.kt
@@ -5,10 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.vocabularyengapplication.adapter.CategoryAdapter
 import com.example.vocabularyengapplication.databinding.ActivityDashboardBinding
+import com.example.vocabularyengapplication.db.SqlDBHandler
+import com.example.vocabularyengapplication.model.WordCategory
 
 class DashboardActivity : AppCompatActivity() {
     private lateinit var bindingDashboard: ActivityDashboardBinding
+    private lateinit var adapterCategory: CategoryAdapter
+    private var selectedCategory: WordCategory = WordCategory.ALL_CATEGORIES
+    private val dbHandler: SqlDBHandler = SqlDBHandler(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,8 +26,34 @@ class DashboardActivity : AppCompatActivity() {
 
         val sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE)
         val userName = sharedPreferences.getString("USER_NAME", null)
-        if(userName!= null){
+        if (userName != null) {
             bindingDashboard.tvGreeting.text = getString(R.string.txt_greeting, userName)
         }
+
+        setCategoryList()
+    }
+
+    fun setCategoryList() {
+        val categoryList = WordCategory.values().toList()
+        adapterCategory = CategoryAdapter(categoryList, selectedCategory) { wordCategory ->
+            selectedCategory = wordCategory
+            refreshListCategoryAndVocab(wordCategory)
+        }
+
+        bindingDashboard.rvCategory.apply {
+            layoutManager =
+                LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+            adapter = adapterCategory
+        }
+    }
+
+    private fun refreshListCategoryAndVocab(wordCategory: WordCategory) {
+        val llistWord = if (wordCategory == WordCategory.ALL_CATEGORIES) {
+            dbHandler.getVocab()
+        } else {
+            dbHandler.getVocab().filter { it.category == wordCategory }
+        }
+
+        adapterCategory.updateSelectedCategory(selectedCategory)
     }
 }
